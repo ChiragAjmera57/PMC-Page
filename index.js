b06fb05974b6dee84a1547a61e4efc0feee180fd
Chirag Ajmera <chiragajmera57@gmail.com>
2023-12-05 12:05:28 +0530

    pmc approved

--- index.js
+++ index.js
@@ -16,3 +16,28 @@ const mouseRemove = (element) =>{
         }
       }
 }
+
+function handleScroll() {
+  var scrollPosition = window.scrollY || window.pageYOffset;
+  var navTextElements = document.getElementsByClassName('change-white');
+  var nav = document.querySelector('nav');
+
+  // Loop through all elements with the class 'change-white'
+  for (var i = 0; i < navTextElements.length; i++) {
+    var navText = navTextElements[i];
+
+    if (scrollPosition >= 72) {
+      // Remove 'text-white' class from each element
+      navText.classList.remove('text-white');
+      nav.classList.remove('custom-nav-bg');
+      nav.classList.add('nav-bg-white');
+    } else {
+      // Add 'text-white' class to each element
+      nav.classList.remove('nav-bg-white');
+      nav.classList.add('custom-nav-bg');
+      navText.classList.add('text-white');
+    }
+  }
+}
+
+window.addEventListener('scroll', handleScroll);
